3f3d1a13d47f960687a81d883ca46ae6788014a9
jaxon <chenjie@huizhaofang.com>
2020-08-24 17:31:47 +0800

    git

--- weather/test.py
+++ weather/test.py
@@ -2,10 +2,12 @@
 @Time    : 2020/8/19 0019 23:05
 @Author  : jaxon
 """
+import os
+
 import requests
 
 
-name = input("input:")
+name = os.getenv("city")
 ## 和风
 url = f"https://geoapi.heweather.net/v2/city/lookup?location={name}&key=d49a4c5afadc47b9b5fc70e4ae7beefa&range=cn"
 a = requests.get(url)
