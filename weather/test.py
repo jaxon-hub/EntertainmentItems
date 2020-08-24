bfedb116aae54f67ae99ed2a29cf1ce113b27d45
jaxon <chenjie@huizhaofang.com>
2020-08-24 17:35:09 +0800

    git

--- weather/test.py
+++ weather/test.py
@@ -8,6 +8,7 @@ import requests
 
 
 name = os.getenv("city")
+print(name,"11111111")
 url = f"https://geoapi.heweather.net/v2/city/lookup?location={name}&key=d49a4c5afadc47b9b5fc70e4ae7beefa&range=cn"
 a = requests.get(url)
 # print(a.content)
