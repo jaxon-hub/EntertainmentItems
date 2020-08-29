31fc8b4bd82bf43578a994934bd28a100c47ea24
Jaxon <42506435+jaxonUK@users.noreply.github.com>
2020-08-29 14:27:26 +0800

    调整输出格式

--- weather/test.py
+++ weather/test.py
@@ -1,20 +1,33 @@
-# -*- coding:utf-8 -*-
 """
 @Time    : 2020/8/19 0019 23:05
 @Author  : jaxon
 """
 import os
-
+import datetime
 import requests
 
 
 name = os.getenv("city")
-print(name,"11111111")
+# name = "三亚"
 url = f"https://geoapi.heweather.net/v2/city/lookup?location={name}&key=d49a4c5afadc47b9b5fc70e4ae7beefa&range=cn"
 a = requests.get(url)
-# print(a.content)
 citycode = a.json()["location"][0]["id"]
 weather_url = f"http://wthrcdn.etouch.cn/weather_mini?citykey={citycode}"
-b = requests.get(weather_url)
-print(b.json())
+r_data = requests.get(weather_url).json()
+city = r_data["data"]["city"]
+ganmao = r_data["data"]["ganmao"]
+weather_data = (r_data["data"]["forecast"])
+cday = datetime.datetime.strptime('2018-9-19 18:19:59', '%Y-%m-%d %H:%M:%S')
+print(f"---------------------------当前时间为:{cday}---------------------------")
+print("|")
+for data in weather_data:
+    date = data["date"]
+    high = data["high"]
+    low = data["low"]
+    fengxiang = data["fengxiang"]
+    type = data["type"]
+    print(f"|                      {date}  {city}天气为：{high},{low},{fengxiang},{type}"), "|"
+    print("|")
+print(f"|                      {ganmao}")
+print("----------------------------------------------------------------------------------")
 
